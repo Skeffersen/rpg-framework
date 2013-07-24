ad657964d196a6694c95cfe67f16c26e643a8f9e
Ryan Kadwell <ryan@riaka.ca>
2013-07-23 22:14:59 -0400

    Added documentation to character and item classes

--- character.cpp
+++ character.cpp
@@ -1,2 +1,19 @@
-class character
-{};
+/**
+ * rpg-framework
+ *
+ * Copyright (c) Derek Kadwell <derek@dmajorstudios.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Representation for characters
+ *
+ * @author Derek Kadwell <derek@dmajorstudios.com>
+ * @author Ryak Kadwell <ryan@riaka.ca>
+ */
+class Character
+{
+
+};
--- item.cpp
+++ item.cpp
@@ -1,2 +1,19 @@
-class item
-{};
+/**
+ * rpg-framework
+ *
+ * Copyright (c) Derek Kadwell <derek@dmajorstudios.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Base class for all items
+ *
+ * @author Derek Kadwell <derek@dmajorstudios.com>
+ * @author Ryak Kadwell <ryan@riaka.ca>
+ */
+class Item
+{
+
+};
